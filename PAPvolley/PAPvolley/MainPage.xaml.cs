91f2ae3f17928b3324252590a6d57c852876178d
Airton Messias <aeffl.21843@hotmail.com>
2020-02-03 16:45:29 +0000

    Add project files.

---
+++ PAPvolley/PAPvolley/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PAPvolley
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Onbtn_Clicked2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ThirdPage());
+        }
+        private async void Onbtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}

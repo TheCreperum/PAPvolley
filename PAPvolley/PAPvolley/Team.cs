e8330080de9bc2f0c0a071ed2a851c0aef731408
Airton Messias <aeffl.21843@hotmail.com>
2020-03-02 18:11:26 +0000

    1.1.0

---
+++ PAPvolley/PAPvolley/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace PAPvolley
+{
+    class Team
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string TeamName { get; set; }
+
+    }
+}
---
+++ PAPvolley/PAPvolley/TeamDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace PAPvolley
+{
+    class TeamDB
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        public bool createDataBase()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    connection.CreateTable<Team>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                string error = ex.Message;
+                return false;
+            }
+        }
+        public bool insertIntoTablePerson(Team team)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Teams.db")))
+                {
+                    connection.Insert(team);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                string error = ex.Message;
+                return false;
+            }
+        }
+    }
+}
